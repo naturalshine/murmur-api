284d9a0801636bdf9f807a5969c7509c618b3a66
Claire Tolan <claire@new-eelam.com>
2023-02-22 22:30:54 +0100

    sample and pack tests

---
+++ test/pack.test.js
@@ -0,0 +1,148 @@
+import { expect, server, BASE_URL } from './setup';
+
+describe('Packs', () => {
+
+
+    it('inserts pack', done => {
+    
+        const data = { 
+          title: 'Claire', 
+          description: 'pack description', 
+          image: '',
+          file: 'deer',
+          path: '/home/cst/code/murmur/murmur-api/src/resources/static/assets/uploads/',
+          authorship: [{
+              created_by: [
+                  {
+                      primary_artist: 'Claire Tolan',
+                      collaborators: ['CST', 'Chez Shhh']
+                  }
+              ],
+              written_by: [
+                  {
+                      primary_artist: 'Claire Tolan',
+                      collaborators: ['CST', 'Chez Shhh']
+                  }
+              ]
+          }],
+          published: true, 
+          duration: '3.01',
+          play_count: 0,
+          keywords: [{
+              keywords: ['cool', 'pack', 'shhh', 'role play']}],
+          asmr_sounds: [{
+              asmr_sounds: ['crinkling', 'scratching', 'whispering']
+          }],
+          lyrics: 'suck it losers'
+      };    
+      server
+        .post(`${BASE_URL}/media/packs`)
+        .send(data)
+        .expect(200)
+        .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.message).to.be.instanceOf(Object);
+        expect(res.body.message[0]).to.have.property('id');
+        expect(res.body.message[0]).to.have.property('title', data.title);
+        expect(res.body.message[0]).to.have.property('description', data.description);
+        done();
+      });
+    });
+
+  it('gets all packs', done => {
+    server
+      .get(`${BASE_URL}/media/packs`)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.messages).to.be.instanceOf(Array);
+        res.body.messages.forEach(m => {
+          expect(m).to.have.property('id');
+          expect(m).to.have.property('title');
+          expect(m).to.have.property('description');
+        });
+        done();
+      });
+  });
+
+  it('gets single pack', done => {
+    server
+      .get(`${BASE_URL}/media/pack?id=1`)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.messages).to.be.instanceOf(Object);
+        res.body.messages.forEach(m => {
+          expect(m).to.have.property('id');
+          expect(m).to.have.property('title');
+          expect(m).to.have.property('description');
+        });
+        done();
+      });
+  });
+  
+  it('creates tableland pack table', done => {
+    const tData = {
+            'id' : 'integer primary key',
+            'name': 'text', 
+            'description': 'text', 
+            'image': 'text', 
+            'attributes': 'text'
+          };
+    server
+        .post(`${BASE_URL}/media/packs/tableland/create`)
+        .send(tData)
+        .expect(200)
+        .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+        });
+    }).timeout(100000);
+
+  it('creates web3 pack', done => {
+      const data = { 
+        title: 'Claire the sixth', 
+        description: 'pack description', 
+        image: '',
+        file: 'deer',
+        path: '/home/cst/code/murmur/murmur-api/src/resources/static/assets/uploads/',
+        authorship: [{
+            created_by: [
+                {
+                    primary_artist: 'Claire Tolan',
+                    collaborators: ['CST', 'Chez Shhh']
+                }
+            ],
+            written_by: [
+                {
+                    primary_artist: 'Claire Tolan',
+                    collaborators: ['CST', 'Chez Shhh']
+                }
+            ]
+        }],
+        published: true, 
+        duration: '3.01',
+        play_count: 0,
+        keywords: [{
+            keywords: ['cool', 'pack', 'shhh', 'role play']}],
+        asmr_sounds: [{
+            asmr_sounds: ['crinkling', 'scratching', 'whispering']
+        }],
+        lyrics: 'suck it losers'
+    };    
+    server
+        .post(`${BASE_URL}/media/packs/nft`)
+        .send(data)
+        .expect(200)
+        .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.message[0]).to.be.instanceOf(Object);
+        expect(res.body.message[0]).to.have.property('id');
+        expect(res.body.message[0]).to.have.property('title', data.title);
+        expect(res.body.message[0]).to.have.property('description', data.description);
+        done();
+        });
+    }).timeout(100000);
+
+
+});
---
+++ test/sample.test.js
@@ -0,0 +1,151 @@
+import { expect, server, BASE_URL } from './setup';
+
+describe('Samples', () => {
+
+
+    it('inserts sample', done => {
+    
+        const data = { 
+          title: 'Claire', 
+          description: 'sample description', 
+          image: '',
+          audio: '',
+          file: 'deer',
+          path: '/home/cst/code/murmur/murmur-api/src/resources/static/assets/uploads/',
+          authorship: [{
+              created_by: [
+                  {
+                      primary_artist: 'Claire Tolan',
+                      collaborators: ['CST', 'Chez Shhh']
+                  }
+              ],
+              written_by: [
+                  {
+                      primary_artist: 'Claire Tolan',
+                      collaborators: ['CST', 'Chez Shhh']
+                  }
+              ]
+          }],
+          published: true, 
+          duration: '3.01',
+          play_count: 0,
+          keywords: [{
+              keywords: ['cool', 'sample', 'shhh', 'role play']}],
+          asmr_sounds: [{
+              asmr_sounds: ['crinkling', 'scratching', 'whispering']
+          }],
+          lyrics: 'suck it losers'
+      };    
+      server
+        .post(`${BASE_URL}/media/samples`)
+        .send(data)
+        .expect(200)
+        .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.message).to.be.instanceOf(Object);
+        expect(res.body.message[0]).to.have.property('id');
+        expect(res.body.message[0]).to.have.property('title', data.title);
+        expect(res.body.message[0]).to.have.property('description', data.description);
+        done();
+      });
+    });
+
+  it('gets all samples', done => {
+    server
+      .get(`${BASE_URL}/media/samples`)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.messages).to.be.instanceOf(Array);
+        res.body.messages.forEach(m => {
+          expect(m).to.have.property('id');
+          expect(m).to.have.property('title');
+          expect(m).to.have.property('description');
+        });
+        done();
+      });
+  });
+
+  it('gets single sample', done => {
+    server
+      .get(`${BASE_URL}/media/sample?id=1`)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.messages).to.be.instanceOf(Object);
+        res.body.messages.forEach(m => {
+          expect(m).to.have.property('id');
+          expect(m).to.have.property('title');
+          expect(m).to.have.property('description');
+        });
+        done();
+      });
+  });
+  
+  it('creates tableland sample table', done => {
+    const tData = {
+            'id' : 'integer primary key',
+            'name': 'text', 
+            'description': 'text', 
+            'audio': 'text',
+            'image': 'text', 
+            'attributes': 'text'
+          };
+    server
+        .post(`${BASE_URL}/media/samples/tableland/create`)
+        .send(tData)
+        .expect(200)
+        .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+        });
+    }).timeout(100000);
+
+  it('creates web3 sample', done => {
+      const data = { 
+        title: 'Claire the sixth', 
+        description: 'sample description', 
+        image: '',
+        audio: '',
+        file: 'deer',
+        path: '/home/cst/code/murmur/murmur-api/src/resources/static/assets/uploads/',
+        authorship: [{
+            created_by: [
+                {
+                    primary_artist: 'Claire Tolan',
+                    collaborators: ['CST', 'Chez Shhh']
+                }
+            ],
+            written_by: [
+                {
+                    primary_artist: 'Claire Tolan',
+                    collaborators: ['CST', 'Chez Shhh']
+                }
+            ]
+        }],
+        published: true, 
+        duration: '3.01',
+        play_count: 0,
+        keywords: [{
+            keywords: ['cool', 'sample', 'shhh', 'role play']}],
+        asmr_sounds: [{
+            asmr_sounds: ['crinkling', 'scratching', 'whispering']
+        }],
+        lyrics: 'suck it losers'
+    };    
+    server
+        .post(`${BASE_URL}/media/samples/nft`)
+        .send(data)
+        .expect(200)
+        .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.message[0]).to.be.instanceOf(Object);
+        expect(res.body.message[0]).to.have.property('id');
+        expect(res.body.message[0]).to.have.property('title', data.title);
+        expect(res.body.message[0]).to.have.property('description', data.description);
+        done();
+        });
+    }).timeout(100000);
+
+
+});
